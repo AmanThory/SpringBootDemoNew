95634996e91700c7c8484d85b9ef7caa936d8199
Aman <aman.kumar@faberwork.com>
2020-03-28 23:16:10 +0530

    First commit

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/home")
+    public ModelAndView home(){
+
+        ModelAndView mv = new ModelAndView("home");
+        System.out.println("====================[ Home Controller ]================");
+        return mv;
+    }
+}
